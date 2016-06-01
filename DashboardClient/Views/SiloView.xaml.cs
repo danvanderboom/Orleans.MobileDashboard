2cad5f30b6c579421559b5a84bcec3df1ffdb79f
Dan Vanderboom <danvanderboom@gmail.com>
2016-06-01 15:09:11 -0600

    updated some colors, whitespace (paddings & margins), and adjusted other misc layout elements; added a color palette view to visualize in previewer during design time

--- DashboardClient/Views/SiloView.xaml.cs
+++ DashboardClient/Views/SiloView.xaml.cs
@@ -51,39 +51,47 @@ namespace DashboardClient.Views
 
 		void UpdateOrientation ()
 		{
+			var columns = 6;
+
 			if (Orientation == StackOrientation.Vertical) 
 			{
-				SiloContent.SetGridColumn (0, 5);
+				SiloContent.SetGridColumn (0, columns);
 				SiloContent.SetGridRow (0, 1);
 
-				VersionContent.SetGridColumn (0, 5);
+				VersionContent.SetGridColumn (0, columns);
 				VersionContent.SetGridRow (1, 1);
 
-				CPUContent.SetGridColumn (0, 5);
+				CPUContent.SetGridColumn (0, columns);
 				CPUContent.SetGridRow (2, 1);
 
-				MemoryContent.SetGridColumn (0, 5);
+				MemoryContent.SetGridColumn (0, columns);
 				MemoryContent.SetGridRow (3, 1);
 
-				GrainContent.SetGridColumn (0, 5);
+				GrainContent.SetGridColumn (0, columns);
 				GrainContent.SetGridRow (4, 1);
+
+				ActivationsContent.SetGridColumn (0, columns);
+				ActivationsContent.SetGridRow (5, 1);
 			} 
 			else 
 			{
 				SiloContent.SetGridColumn (0, 1);
-				SiloContent.SetGridRow (0, 5);
+				SiloContent.SetGridRow (0, columns);
 
 				VersionContent.SetGridColumn (1, 1);
-				VersionContent.SetGridRow (0, 5);
+				VersionContent.SetGridRow (0, columns);
 
 				CPUContent.SetGridColumn (2, 1);
-				CPUContent.SetGridRow (0, 5);
+				CPUContent.SetGridRow (0, columns);
 
 				MemoryContent.SetGridColumn (3, 1);
-				MemoryContent.SetGridRow (0, 5);
+				MemoryContent.SetGridRow (0, columns);
 
 				GrainContent.SetGridColumn (4, 1);
-				GrainContent.SetGridRow (0, 5);
+				GrainContent.SetGridRow (0, columns);
+
+				ActivationsContent.SetGridColumn (5, 1);
+				ActivationsContent.SetGridRow (0, columns);
 			}
 		}
 	}
