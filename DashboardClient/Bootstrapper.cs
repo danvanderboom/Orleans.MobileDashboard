94a25afc5d76972ccc2a0d7bf8d7b466aed9a342
Dan <danvanderboom@gmail.com>
2016-06-05 13:42:32 -0600

    got Prism working in the UWP app; switched to latest stable Xamarin.Forms

--- DashboardClient/Bootstrapper.cs
+++ DashboardClient/Bootstrapper.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using Prism.Unity;
-using Xamarin.Forms;
+﻿//using System;
+//using Prism.Unity;
+//using Xamarin.Forms;
 
-namespace DashboardClient
-{
-	public class Bootstrapper : UnityBootstrapper
-	{
-		protected override void OnInitialized()
-		{
-		}
+//namespace DashboardClient
+//{
+//	public class Bootstrapper : UnityBootstrapper
+//	{
+//		protected override void OnInitialized()
+//		{
+//		}
 
-		protected override Xamarin.Forms.Page CreateMainPage ()
-		{
-			//return (Page)Container.Resolve<MasterPage> ();
-			return new MasterPage (Container);
-		}
+//		protected override Xamarin.Forms.Page CreateMainPage ()
+//		{
+//			//return (Page)Container.Resolve<MasterPage> ();
+//			return new MasterPage (Container);
+//		}
 
-		protected override void RegisterTypes ()
-		{
-			Container.RegisterTypeForNavigation<MasterPage> ();
-		}
-	}
-}
+//		protected override void RegisterTypes ()
+//		{
+//			Container.RegisterTypeForNavigation<MasterPage> ();
+//		}
+//	}
+//}
